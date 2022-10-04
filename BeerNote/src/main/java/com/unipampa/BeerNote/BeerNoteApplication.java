763b6d0913f8fbc1ac97b7c08e25b91c0ead32f7
Denner <dennerramiror@gmail.com>
2022-10-03 21:20:56 -0300

    initial commit

---
+++ BeerNote/src/main/java/com/unipampa/BeerNote/BeerNoteApplication.java
@@ -0,0 +1,13 @@
+package com.unipampa.BeerNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerNoteApplication.class, args);
+	}
+
+}
