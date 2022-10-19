ee9bce6cb7d9ae336f2024631e92075eaa91b495
Denner <dennerramiror@gmail.com>
2022-10-19 19:39:20 -0300

    Pessoas model

---
+++ Pessoas/src/main/java/com/unipampa/Pessoas/PessoasApplication.java
@@ -0,0 +1,15 @@
+package com.unipampa.Pessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoasApplication.class, args);
+	}
+
+}
---
+++ Pessoas/src/main/java/com/unipampa/Pessoas/model/Pessoas.java
@@ -0,0 +1,59 @@
+package com.unipampa.Pessoas.model;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "pessoas")
+public class Pessoas implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String name;
+
+    @Column(name = "user", nullable = false)
+    private String user;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
