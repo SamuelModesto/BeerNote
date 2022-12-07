6da903674b11eb9b51567de9a8cfe065cfa2cd9b
Samuel Modesto <samuelmodestoes@gmail.com>
2022-12-07 19:59:03 -0300

    add lombok in project

--- BeerNote/src/main/java/com/unipampa/BeerNote/dto/BeerDto.java
+++ BeerNote/src/main/java/com/unipampa/BeerNote/dto/BeerDto.java
@@ -1,33 +1,10 @@
 package com.unipampa.BeerNote.dto;
 
+import lombok.Data;
+
+@Data
 public class BeerDto {
 
-
     private String name;
-
     private String description;
-
-    public BeerDto() {
-    }
-
-    public BeerDto(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- BeerNote/src/main/java/com/unipampa/BeerNote/model/Beer.java
+++ BeerNote/src/main/java/com/unipampa/BeerNote/model/Beer.java
@@ -1,5 +1,7 @@
 package com.unipampa.BeerNote.model;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
 import javax.persistence.Column;
@@ -11,6 +13,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "beer")
+@Data
 public class Beer implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -24,76 +27,6 @@ public class Beer implements Serializable {
 
     @Column(name = "description")
     private String description;
-
-    public Beer() {
-    }
-
-    public Beer(Integer id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((description == null) ? 0 : description.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Beer other = (Beer) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        if (description == null) {
-            if (other.description != null)
-                return false;
-        } else if (!description.equals(other.description))
-            return false;
-        return true;
-    }
 
     @Override
     public String toString() {
