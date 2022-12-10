7ad82ae14143d7d746fbd3d25ea3987710d0011e
Samuel Modesto <samuelmodestoes@gmail.com>
2022-12-10 15:25:45 -0300

    create model beer and evaluation

---
+++ evaluation/src/main/java/com/unipampa/evaluation/model/Beer.java
@@ -0,0 +1,24 @@
+package com.unipampa.evaluation.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Beer {
+
+    @Id
+    private String id;
+
+    @NonNull
+    private String name;
+
+    @NonNull
+    private String description;
+}
